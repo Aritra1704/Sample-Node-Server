fdfbea6f4a0cd01abab44a7915fd044d0e36d329
aritrapal <aritra.pal@zippr.in>
2018-02-15 20:26:47 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + "/public"));
+
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+
+    next();
+
+    // fs.appendFile('server.log', log + '\n');
+
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to server');
+        }
+    });
+});
+
+//**************Rendering Maintenance******************//
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello express!</h1>');
+    // res.send({
+    //     name: 'Aritra',
+    //     likes: [
+    //         'Reading',
+    //         'Coin collection'
+    //     ]
+    // });
+    res.render('home.hbs', {
+        pageTitle: 'Home page',
+        welcomeMessage: 'Welcome to my website'
+    });
+    //
+});
+
+app.get('/about', (req, res) => {
+    // res.send('About page!');
+    res.render('about.hbs', {
+        pageTitle: 'About page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        err: 'Unable to fullfill this request'
+    });
+});
+
+// app.listen(3000);
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`);
+});
